e81a0c7a79d42f2a15cbdb25bab775d05e62974f
Thorsten Rhau <thorsten@rhau.se>
2025-01-17 22:03:46 +0100

    (util) added 'stow_all.sh' to link all packages

---
+++ stow_all.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# This script links all dot files with the GNU stow command
+
+if ! command -v stow >/dev/null; then
+  echo "Error: 'stow' command not found. Please install it to continue."
+  exit 1
+fi
+
+packages="
+bat
+fish
+ghostty
+git
+lazygit
+starship
+yazi
+zsh
+"
+
+for package in $packages; do
+  stow --target "$HOME" --restow -v "$package"
+done
