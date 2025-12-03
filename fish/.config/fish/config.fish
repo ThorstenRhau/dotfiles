e94d74236071b328fde367be9db34497a0314d2b
Thorsten Rhau <thorsten@rhau.se>
2025-12-03 10:46:21 +0100

    (fish) bat themes and helper function for appearance

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -27,10 +27,37 @@ if status is-login; or status is-interactive
     set -gx fish_greeting
     set -gx fish_history_limit 10000
 
+    # Helper function for system appearance
+    function get_appearance
+        if test (uname) = "Darwin"
+            set -l val (defaults read -g AppleInterfaceStyle 2>/dev/null)
+            if test "$val" = "Dark"
+                echo "dark"
+            else
+                echo "light"
+            end
+        else
+            echo "dark" # Default
+        end
+    end
+
     # Eza (ls replacement)
     if type -q eza
         function ls --wraps eza --description "ls using eza with git info"
             eza --header --git $argv
+        end
+    end
+
+    # Bat (cat replacement)
+    if type -q bat
+        function cat --wraps bat --description "cat using bat"
+            bat --binary no-printing --plain  $argv
+        end
+
+        if test (get_appearance) = "dark"
+            set -gx BAT_THEME "Catppuccin Mocha"
+        else
+            set -gx BAT_THEME "Catppuccin Latte"
         end
     end
 
