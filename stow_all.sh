77d41e4a52b33a2109d230af995c447c66235599
Thorsten Rhau <thorsten@rhau.se>
2025-12-04 20:35:20 +0100

    (stow_all) re-formatted file

--- stow_all.sh
+++ stow_all.sh
@@ -2,9 +2,9 @@
 
 # This script links all dot files with the GNU stow command
 
-if ! command -v stow > /dev/null; then
-  echo "Error: 'stow' command not found. Please install it to continue."
-  exit 1
+if ! command -v stow >/dev/null; then
+    echo "Error: 'stow' command not found. Please install it to continue."
+    exit 1
 fi
 
 packages="
@@ -18,5 +18,5 @@ zsh
 "
 
 for package in $packages; do
-  stow --target "$HOME" --restow -v "$package"
+    stow --target "$HOME" --restow -v "$package"
 done
