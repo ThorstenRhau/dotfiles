df2c9ce056509bd02a0505d9da026f924f7f3858
Thorsten Rhau <thorsten@rhau.se>
2026-01-10 09:38:13 +0100

    docs(fish): added line directive and update comments

--- fish/.config/fish/functions/_check_appearance_on_prompt.fish
+++ fish/.config/fish/functions/_check_appearance_on_prompt.fish
@@ -1,3 +1,4 @@
+# @fish-lsp-disable-next-line 4007
 function _check_appearance_on_prompt --on-event fish_prompt
     # Check if we are on macOS
     if test $_OS_TYPE != Darwin
@@ -12,8 +13,8 @@ function _check_appearance_on_prompt --on-event fish_prompt
         set -g _appearance_last_check $current_time
 
         # Run check in background
-        # We redirect all output to null to ensure no ghost output on the prompt
-        # We unconditionally set the variable; Fish only triggers events if the value actually changes.
+        # Redirect all output to null to ensure no ghost output on the prompt
+        # Unconditionally set the variable; Fish only triggers events if the value actually changes.
         fish -c '
             set -l val (defaults read -g AppleInterfaceStyle 2>/dev/null)
             if test "$val" = "Dark"
