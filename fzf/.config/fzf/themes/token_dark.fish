771f1da29ed4fc543ee2d55a864799fbdc688987
Thorsten Rhau <thorsten@rhau.se>
2026-04-08 20:09:43 +0200

    refactor(themes): rebalance token-dark palette colors

--- fzf/.config/fzf/themes/token_dark.fish
+++ fzf/.config/fzf/themes/token_dark.fish
@@ -1,9 +1,9 @@
 # Token dark theme for FZF (synced from token contrib)
 
 set -gx _FZF_THEME_OPTS "\
---color=fg:#e8e4dc,bg:#262624,hl:#d17c61 \
---color=fg+:#e8e4dc,bg+:#333331,hl+:#d17c61 \
---color=border:#878681,header:#719fc7,gutter:#262624 \
---color=spinner:#c99565,info:#938e87 \
---color=pointer:#d17c61,marker:#70b36b,prompt:#d17c61
+--color=fg:#e8e4dc,bg:#262624,hl:#bb8777 \
+--color=fg+:#e8e4dc,bg+:#333331,hl+:#bb8777 \
+--color=border:#878681,header:#819eb7,gutter:#262624 \
+--color=spinner:#b59579,info:#938e87 \
+--color=pointer:#bb8777,marker:#7ea37c,prompt:#bb8777
 "
